659d45f1c8e64fcb4f015d555594f5d6b3fb0530
wojtas7p <wojtas7p@gmail.com>
2025-01-28 01:12:21 +0100

    updata

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { SWRConfig } from "swr";
+import axios from "axios";
 
-createRoot(document.getElementById('root')).render(
+import Layout from "./components/Layout/index.jsx";
+import Home from "./routes/Home.jsx";
+import Categories from "./routes/Categories.jsx";
+import Category from "./routes/Category.jsx";
+import Account from "./routes/Account.jsx";
+import fetcher from "./fetcher.js";
+
+axios.defaults.baseURL = "https://fakestoreapi.com";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider>
+      <SWRConfig value={{ fetcher }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/categories/:category" element={<Category />} />
+              <Route path="/account" element={<Account />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </SWRConfig>
+    </Provider>
+  </StrictMode>
+);
---
+++ src/routes/Categories.jsx
@@ -0,0 +1,71 @@
+import {Flex, Spinner, Alert, Box, Card, Image, SimpleGrid} from "@chakra-ui/react";
+import useSWR from "swr";
+import { Link } from "react-router";
+import { categoryImages } from "@/data/category";
+
+const Categories = () => {
+  const { data: categories, error, isLoading } = useSWR("/products/categories");
+
+  return (
+    <Flex
+      gap={5}
+      flexDir={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      paddingTop={10}
+      paddingBottom={10}
+    >
+      {isLoading && <Spinner size="2xl" />}
+
+      {error && (
+        <Alert.Root status="error" maxWidth={"30%"}>
+          <Alert.Indicator />
+          <Alert.Content>
+            <Alert.Title>Server Error</Alert.Title>
+            <Alert.Description>Sorry for that...</Alert.Description>
+          </Alert.Content>
+        </Alert.Root>
+      )}
+
+      {categories?.length > 0 && (
+        <SimpleGrid columns={2} gap={19}>
+          {categories?.map((category) => (
+            <Link key={category} to={`/categories/${category}`}>
+              <Card.Root
+                flexDirection="row"
+                width="xl"
+                shadow={"md"}
+                border="none"
+                _hover={{
+                  transform: "scale(1.05)",
+                  cursor: "pointer",
+                }}
+                transition={"all ease-in-out 0.3s"}
+              >
+                <Image
+                  objectFit="cover"
+                  maxWidth="200px"
+                  src={categoryImages?.[category]}
+                  alt="category image"
+                />
+                <Box>
+                  <Card.Body backgroundColor={"white"}>
+                    <Card.Title color="black" marginBottom="2" fontSize={"xl"}>
+                      {category.toUpperCase()}
+                    </Card.Title>
+                    <Card.Description color="gray.500">
+                      Cras et tempus magna. Phasellus accumsan lorem id pretium
+                      gravida. Sed ante nisl, faucibus condimentum tincidunt no.
+                    </Card.Description>
+                  </Card.Body>
+                </Box>
+              </Card.Root>
+            </Link>
+          ))}
+        </SimpleGrid>
+      )}
+    </Flex>
+  );
+};
+
+export default Categories;
